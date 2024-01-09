b54613f09c3b880c0dff2ec47df493b296c1ba7c
Ardian <ardianiqbal40@gmail.com>
2024-01-09 16:40:27 +0700

    finish

--- input_mahasiswa.py
+++ input_mahasiswa.py
@@ -1,11 +1,10 @@
-
-data = [['Nama','NIM','Tempat Lahir', 'Tanggal Lahir', 'Angkatan'],
-       ['Irsyad','0048371345','Ternate','14','2020']]
+data = [['Nama','NIM','Tempat Lahir', 'Tanggal Lahir', 'Angkatan', 'Jurusan'],
+       ['Irsyad','6082001153','Ternate','14-01-2002','2020','Administrasi Bisnis']]
 
 def main():
-    
+
     programMenyala = True
-    
+
     while (programMenyala):
         print("\n")
         listMenu()
@@ -21,8 +20,10 @@ def main():
                 updateNamaBaseOnNim()
             case "5":
                 exit()
-    
-    
+            case _:
+                print("Perintah Tidak Ditemukan!")
+
+
 def listMenu():
     print("Menu:")
     print("1. Tampil")
@@ -30,14 +31,14 @@ def listMenu():
     print("3. Hapus Mahasiswa")
     print("4. Update Nama Mahasiswa")
     print("5. Keluar")
-    
+
 def tampilData():
     print("\n")
     for i in range(len(data)):
         for j in range(len(data[i])):
             print(f"{data[i][j]:<{15}}" ,end=' ')
         print()
-    
+
 def inputMahasiswa():
     print("\n")
     inputNamaMahasiswa = input("Masukan Nama Mahasiswa: ")
@@ -45,71 +46,74 @@ def inputMahasiswa():
     inputTempatLahir = input("Masukan Tempat Lahir Mahasiswa: ")
     inputTanggalLahir = input("Masukan Tanggal Lahir Mahasiswa: ")
     inputAngkatan = input("Masukan Angkatan Mahasiswa: ")
-    
-    dataMasuk = [inputNamaMahasiswa, inputNIMMahasiswa, inputTempatLahir, inputTanggalLahir, inputAngkatan]
+    inputJurusan = input("Masukan Jurusan Mahasiswa: ")
+
+    dataMasuk = [inputNamaMahasiswa, inputNIMMahasiswa, inputTempatLahir, inputTanggalLahir, inputAngkatan, inputJurusan]
     data.append(dataMasuk)
-    
+
 def hapusMahasiswa():
     print("\n")
     pilihNamaMahasiswa = input("Masukan NIM Mahasiswa yang ingin dihapus: ")
     jumlahIndexTotalMahasiswa = len(data)
     indexDataTerpilih = ''
     namaMahasiswaDitemukan = False
-    
+
     for i in range(0,jumlahIndexTotalMahasiswa):
         for z in range(5):
             if data[i][z] == pilihNamaMahasiswa:
-                dataKode = data[i][0]
-                dataNama = data[i][1]
-                dataQuantity = data[i][2]
-                dataJadwalkeluar = data[i][3]
-                dataJadwalMasuk = data[i][4]
-                dataTmp = [[dataKode, dataNama, dataQuantity, dataJadwalkeluar, dataJadwalMasuk]]
-                for i in range(len(dataTmp)):
-                    for j in range(len(dataTmp[i])):
-                        print(f"{dataTmp[i][j]:<{15}}" ,end=' ')
+                dataNama = data[i][0]
+                dataNIM = data[i][1]
+                dataTempatLahir = data[i][2]
+                dataTanggalLahir = data[i][3]
+                dataAngkatan = data[i][4]
+                dataJurusan = data[i][5]
+                dataTmp = [[dataNama, dataNIM, dataTempatLahir, dataTanggalLahir, dataAngkatan, dataJurusan]]
+                for x in range(len(dataTmp)):
+                    for j in range(len(dataTmp[x])):
+                        print(f"{dataTmp[x][j]:<{15}}" ,end=' ')
                     print()
                 # print(tabulate(dataTmp, headers=header, tablefmt="fancy_grid"))
                 namaMahasiswaDitemukan = True
-                indexDataTerpilih = z + 1
-                
+                indexDataTerpilih = i
+
     if namaMahasiswaDitemukan:
         data.pop(indexDataTerpilih)
         print("Mahasiswa Berhasil Dihapus")
     else:
         print("Kode Tidak Ditemukan")
-     
-    
+
+
 def updateNamaBaseOnNim():
     print("\n")
-    pilihNIMMahasiswa = input("Masukan NIM Mahasiswa: ")
+    pilihNPMMahasiswa = input("Masukan NIM Mahasiswa: ")
     jumlahIndexTotalBarang = len(data)
     indexDataTerpilih = ''
     kodeDitemukan = False
-    
+
     for i in range(0,jumlahIndexTotalBarang):
         for z in range(5):
-            if data[i][z] == pilihNIMMahasiswa:
-                dataKode = data[i][0]
-                dataNama = data[i][1]
-                dataQuantity = data[i][2]
-                dataJadwalkeluar = data[i][3]
-                dataJadwalMasuk = data[i][4]
-                dataTmp = [[dataKode, dataNama, dataQuantity, dataJadwalkeluar, dataJadwalMasuk]]
-                for i in range(len(dataTmp)):
-                    for j in range(len(dataTmp[i])):
-                        print(f"{dataTmp[i][j]:<{15}}" ,end=' ')
+            if data[i][z] == pilihNPMMahasiswa:
+                dataNama = data[i][0]
+                dataNIM = data[i][1]
+                dataTempatLahir = data[i][2]
+                dataTanggalLahir = data[i][3]
+                dataAngkatan = data[i][4]
+                dataJurusan = data[i][5]
+                dataTmp = [[dataNama, dataNIM, dataTempatLahir, dataTanggalLahir, dataAngkatan, dataJurusan]]
+                for x in range(len(dataTmp)):
+                    for j in range(len(dataTmp[x])):
+                        print(f"{dataTmp[x][j]:<{15}}" ,end=' ')
                     print()
                 kodeDitemukan = True
                 inputNamaBaru = input("Masukan Nama Baru: ")
-                indexDataTerpilih = i
-                
+                indexDataTerpilih = i 
+
     if kodeDitemukan:
         data[indexDataTerpilih][0] = inputNamaBaru
     else:
         print("Kode Tidak Ditemukan")
 
-    
+
 
 if __name__ == "__main__":
     main()
